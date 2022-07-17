71820774b80c5d5d3b3a6892847080578f6a6f46
Macharia Muguku <machariamuguku@gmail.com>
2022-07-17 23:17:29 +0300

    use chalk for errors and warnings

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
+import chalk from "chalk";
+
+const error = chalk.bold.red;
+const warning = chalk.hex("#FFA500");
 
 const allArguments = process.argv;
 const myArguments: string[] = process.argv.slice(2);
 
 if (!myArguments.length) {
-  console.log("Please pass in a city to picnic in");
+  console.log(error("Please pass in a city to picnic in"));
   process.exit(1);
 }
 if (myArguments.length > 1) {
   console.log(
-    "You've passed in more than one city. We'll attempt to use the first one"
+    warning(
+      `You've passed in more than one city. We'll attempt to use the first one. ('${myArguments[0]}')`
+    )
   );
 }
