3093b98fd9c4a03ededfc1fa54f11539c0790604
Macharia Muguku <machariamuguku@gmail.com>
2022-07-17 22:45:25 +0300

    handle none or more than one args

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
 const allArguments = process.argv;
 const myArguments: string[] = process.argv.slice(2);
+
+if (!myArguments.length) {
+  console.log("Please pass in a city to picnic in");
+}
+if (myArguments.length > 1) {
+  console.log(
+    "You've passed in more than one city. We'll attempt to use the first one"
+  );
+}
