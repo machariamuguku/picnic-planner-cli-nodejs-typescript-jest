883822b3feddc274b87a98cc5be6e5b47054929a
Macharia Muguku <machariamuguku@gmail.com>
2022-07-18 00:55:56 +0300

    rename chalk error function

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 import chalk from "chalk";
 
-const error = chalk.bold.red;
-const warning = chalk.hex("#FFA500");
+const errorFn = chalk.bold.red;
+const warningFn = chalk.hex("#FFA500");
 
 const allArguments = process.argv;
 const myArguments: string[] = allArguments.slice(2);
 
 if (!myArguments.length) {
-  console.log(error("Please pass in a city to picnic in"));
+  console.log(errorFn("Please pass in a city to picnic in"));
   process.exit(1);
 }
 if (myArguments.length > 1) {
   console.log(
-    warning(
+    warningFn(
       `You've passed in more than one city. We'll attempt to use the first one. ('${myArguments[0]}')`
     )
   );
