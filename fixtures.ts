4a9a4697805735bf72ada03c11b7c8288835c061
Macharia Muguku <machariamuguku@gmail.com>
2022-07-19 04:58:41 +0300

    unit tests for core functions, sample fixture data

---
+++ fixtures.ts
@@ -0,0 +1,51 @@
+export const sampleLondonResp = {
+  queryCost: 1,
+  latitude: 51.5064,
+  longitude: -0.12721,
+  resolvedAddress: "London, England, United Kingdom",
+  address: "london",
+  timezone: "Europe/London",
+  tzoffset: 1,
+  days: [
+    {
+      datetime: "2022-07-23",
+      temp: 20.2,
+      precip: 0,
+      windspeed: 15.5,
+    },
+    {
+      datetime: "2022-07-24",
+      temp: 23.2,
+      precip: 0,
+      windspeed: 20.9,
+    },
+  ],
+};
+
+export const sampleNairobiResp = {
+  queryCost: 1,
+  latitude: -1.28352,
+  longitude: 36.8238,
+  resolvedAddress: "Nairobi, Kenya",
+  address: "nairobi",
+  timezone: "Africa/Nairobi",
+  tzoffset: 3,
+  days: [
+    { datetime: "2022-07-23", temp: 16.5, precip: 0.1, windspeed: 16.9 },
+    { datetime: "2022-07-24", temp: 16.4, precip: 0.1, windspeed: 13.3 },
+  ],
+};
+
+export const sampleMarrakechResp = {
+  queryCost: 1,
+  latitude: 31.6338,
+  longitude: -8.00222,
+  resolvedAddress: "مراكش, مراكش - آسفي, المغرب",
+  address: "marrakech",
+  timezone: "Africa/Casablanca",
+  tzoffset: 1,
+  days: [
+    { datetime: "2022-07-23", temp: 36.3, precip: 0, windspeed: 37.1 },
+    { datetime: "2022-07-24", temp: 38.4, precip: 0.1, windspeed: 23.8 },
+  ],
+};
---
+++ picnic-planner.test.ts
@@ -0,0 +1,109 @@
+import fetch from "node-fetch";
+import {
+  queryNextWeekendWeatherForecast,
+  isCold,
+  isExpectedToRain,
+  getDayNameFromDateTime,
+  processWeatherData,
+  chalkSuccess,
+} from "./picnic-planner";
+import {
+  sampleLondonResp,
+  sampleNairobiResp,
+  sampleMarrakechResp,
+} from "./fixtures";
+
+const { Response } = jest.requireActual("node-fetch");
+jest.mock("node-fetch", () => jest.fn());
+
+global.console = {
+  ...console,
+  log: jest.fn(),
+};
+
+describe("picnic-planner-unit-tests", () => {
+  const apiKey = "API_KEY";
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns correct data given correct city and api key", async () => {
+    (fetch as jest.MockedFunction<typeof fetch>).mockResolvedValueOnce(
+      new Response(JSON.stringify(sampleLondonResp))
+    );
+
+    const res = await queryNextWeekendWeatherForecast("london", apiKey);
+    expect(res).toEqual(sampleLondonResp);
+  });
+
+  it("returns error message if response not ok", async () => {
+    (fetch as jest.MockedFunction<typeof fetch>).mockRejectedValueOnce(
+      "some error"
+    );
+    const errorSpy = jest.fn();
+
+    const res = await queryNextWeekendWeatherForecast("london", apiKey).catch(
+      (err) => {
+        errorSpy(err);
+      }
+    );
+    expect(errorSpy).toHaveBeenCalledTimes(1);
+    expect(errorSpy).toHaveBeenCalledWith("some error");
+  });
+
+  it("isCold", () => {
+    expect(isCold(9)).toBeTruthy();
+    expect(isCold(12)).toBeFalsy();
+  });
+
+  it("isExpectedToRain", () => {
+    expect(isExpectedToRain(9)).toBeTruthy();
+    expect(isExpectedToRain(0)).toBeFalsy();
+  });
+
+  it("getDayNameFromDateTime", () => {
+    expect(getDayNameFromDateTime("2022-07-23")).toEqual("Saturday");
+    expect(getDayNameFromDateTime("2022-07-24")).toEqual("Sunday");
+  });
+
+  it("processWeatherData - no picnic day", () => {
+    jest.spyOn(process, "exit").mockImplementation();
+    const consoleSpy = jest.spyOn(global.console, "log");
+
+    processWeatherData(sampleNairobiResp);
+
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalkSuccess(
+        "The weather isn’t looking very good this weekend, maybe stay indoors."
+      )
+    );
+  });
+
+  it("processWeatherData - one picnic day", () => {
+    jest.spyOn(process, "exit").mockImplementation();
+    const consoleSpy = jest.spyOn(global.console, "log");
+
+    processWeatherData(sampleMarrakechResp);
+
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalkSuccess("You should have your picnic on Saturday.")
+    );
+  });
+
+  it("processWeatherData - two picnic days", () => {
+    jest.spyOn(process, "exit").mockImplementation();
+    const consoleSpy = jest.spyOn(global.console, "log");
+
+    processWeatherData(sampleLondonResp);
+
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+    expect(consoleSpy).toHaveBeenCalledWith(
+      chalkSuccess(
+        "This weekend looks nice for a picnic, Saturday is best because it’s less windy."
+      )
+    );
+  });
+});
